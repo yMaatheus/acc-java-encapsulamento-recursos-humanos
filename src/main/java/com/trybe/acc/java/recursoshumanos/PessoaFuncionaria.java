fd6a93477d0c33c93984a3a0d0227aa4383af2c6
Matheus <ymatheus.h@gmail.com>
2023-07-19 15:39:55 -0300

    feat: implements class PessoaFuncionaria.java with attributes nomeCompleto, cpf, endereço, salario

--- src/main/java/com/trybe/acc/java/recursoshumanos/PessoaFuncionaria.java
+++ src/main/java/com/trybe/acc/java/recursoshumanos/PessoaFuncionaria.java
@@ -4,5 +4,43 @@ package com.trybe.acc.java.recursoshumanos;
  * Classe que representa uma pessoa funcionária no sistema de recursos humanos.
  */
 public class PessoaFuncionaria {
+  private String nomeCompleto;
+  private final String cpf;
+  private String endereco;
+  private double salario;
 
+  PessoaFuncionaria(String nomeCompleto, String cpf, String endereco, double salario) {
+    this.nomeCompleto = nomeCompleto;
+    this.cpf = cpf;
+    this.endereco = endereco;
+    this.salario = salario;
+  }
+
+  public String getNomeCompleto() {
+    return nomeCompleto;
+  }
+
+  public void setNomeCompleto(String nomeCompleto) {
+    this.nomeCompleto = nomeCompleto;
+  }
+
+  public String getCpf() {
+    return cpf;
+  }
+
+  public String getEndereco() {
+    return endereco;
+  }
+
+  public void setEndereco(String endereco) {
+    this.endereco = endereco;
+  }
+
+  public double getSalario() {
+    return salario;
+  }
+
+  public void setSalario(double salario) {
+    this.salario = salario;
+  }
 }
